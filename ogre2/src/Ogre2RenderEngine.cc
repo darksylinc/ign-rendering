0b26741d85a83e6d845ed867acc297aa2e6431d1
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-06-18 17:42:54 -0300

    Fix bad merge errors
    Signed-off-by: Matias N. Goldberg <dark_sylinc@yahoo.com.ar>

--- ogre2/src/Ogre2RenderEngine.cc
+++ ogre2/src/Ogre2RenderEngine.cc
@@ -1234,12 +1234,6 @@ NativeWindowPtr Ogre2RenderEngine::CreateNativeWindow(
 }
 
 //////////////////////////////////////////////////
-GraphicsAPI Ogre2RenderEngine::CurrentGraphicsAPI() const
-{
-  return this->dataPtr->graphicsAPI;
-}
-
-//////////////////////////////////////////////////
 void Ogre2RenderEngine::InitAttempt()
 {
   this->initialized = false;
